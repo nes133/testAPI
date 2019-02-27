af1ce288d5ea6a068140fb6bb7cd568ffb61931b
Marko Kipi <markokipi@yahoo.com>
2019-02-27 18:53:20 +0100

    Picture Mixer

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+function getPictures() {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((res) => res.json())
+    .then((data) => {
+    let output = '<h2>Colors</h2>';
+    data.forEach(function(pic) {
+      output += `
+      <div>
+            <h3>${pic.title}</h3>
+            <img src="${pic.url}" width="30" height="30">
+
+      </div>
+      `  
+    })
+    document.getElementById('output').innerHTML = output;
+    }) 
+}
+
+getPictures();
